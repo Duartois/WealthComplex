b7e85900566b4bcde4de1e022189fd48fa412a25
Matheus Duarte <145275539+Duartois@users.noreply.github.com>
2025-10-30 12:06:12 -0300

    Localize projects statistics

--- src/components/sections/projects/projects.jsx
+++ src/components/sections/projects/projects.jsx
@@ -1,32 +1,24 @@
+import { useTranslation } from 'react-i18next';
 import './projects.scss';
 
-const Projects = () => (
-  <section id="projects" className="projects">
-    <div className="projects__container">
-      <div className="projects__grid" role="list">
-        <article className="projects__item" role="listitem">
-          <h3 className="text-h2 text-primary">
-            3 million+
-          </h3>
-          <p className="projects__description ">
-            Canadians trust us to manage and grow their wealth
-          </p>
-        </article>
-        <article className="projects__item" role="listitem">
-          <h3 className="text-h2 text-primary">$100+ billion</h3>
-          <p className="projects__description">
-            Assets under administration, and growing every single day
-          </p>
-        </article>
-        <article className="projects__item" role="listitem">
-          <h3 className="text-h2 text-primary">$1 million</h3>
-          <p className="projects__description">
-            Eligible coverage with CDIC for chequing and CIPF for investments
-          </p>
-        </article>
+const Projects = () => {
+  const { t } = useTranslation();
+  const stats = t('projects.stats', { returnObjects: true });
+
+  return (
+    <section id="projects" className="projects">
+      <div className="projects__container">
+        <div className="projects__grid" role="list">
+          {stats.map((stat, index) => (
+            <article className="projects__item" role="listitem" key={index}>
+              <h3 className="text-h2 text-primary">{stat.value}</h3>
+              <p className="projects__description">{stat.description}</p>
+            </article>
+          ))}
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Projects;
